dc6896ebcdef5b0e1d296b689deaa6390067778f
Doug Freed <dwfreed@mtu.edu>
2015-12-18 12:20:04 +0400

    Oops, missed a couple references to $block

--- cgi-bin/secret/logs.pl
+++ cgi-bin/secret/logs.pl
@@ -18,8 +18,8 @@ $index = int $index;
 my $i = int($index / 10000) + 1;
 
 if ( -e "/var/www/actionlogs/${i}0K.tar.gz") {
-  print "tar -Oxf /var/www/actionlogs/$block.tar.gz $index.txt\n\n";
-  print `tar -Oxf /var/www/actionlogs/$block.tar.gz $index.txt`;
+  print "tar -Oxf /var/www/actionlogs/${i}0K.tar.gz $index.txt\n\n";
+  print `tar -Oxf /var/www/actionlogs/${i}0K.tar.gz $index.txt`;
 } elsif ( -e "/var/www/actionlogs/$index.txt.lzma" ) {
   print `lzcat /var/www/actionlogs/$index.txt.lzma`;
 } elsif ( -e "/var/www/actionlogs/$index.txt" ) {
