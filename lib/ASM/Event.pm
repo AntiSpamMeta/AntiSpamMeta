AntiSpamMeta/AntiSpamMeta

commit ed53ab7178841862af88f05fafe56a7b8504d527

Ignore channels that are +s and not monitored when making actionlogs

lib/ASM/Event.pm
@@ -290,7 +290,8 @@ sub on_part
   my $chan = lc $event->{to}->[0];
   $::log->logg( $event );
   $::db->logg( $event ) if defined $::db;
-  if (defined $::db and $event->{args}->[0] =~ /^requested by/) {
+  # Ignore channels that are +s and not monitored
+  if (defined $::db and $event->{args}->[0] =~ /^requested by/ and (not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no"))) ) {
     my $idx = $::db->actionlog( $event);
     $::log->sqlIncident($chan, $idx) if $idx;
   }
@@ -406,7 +407,9 @@ sub on_quit
   $event->{to} = \@channels;
   if (defined $::db) {
       my $idx = $::db->actionlog($event);
-      $::log->sqlIncident( join(',', @channels), $idx ) if $idx;
+      # Ignore channels that are +s and not monitored
+      my @actionlog_channels = grep { not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no")) } @channels;
+      $::log->sqlIncident( join(',', @actionlog_channels), $idx ) if $idx;
       $::db->logg( $event );
   }
   $::log->logg( $event );
@@ -536,8 +539,11 @@ sub on_kick {
   $::log->logg( $event );
   if (defined $::db) {
       $::db->logg( $event );
-      my $idx = $::db->actionlog($event);
-      $::log->sqlIncident($chan, $idx) if $idx;
+      # Ignore channels that are +s and not monitored
+      if( not ((grep { /^s$/ } @{$::sc{$chan}{modes}}) && ($::channels->{channel}->{$chan}->{monitor} eq "no")) ) {
+          my $idx = $::db->actionlog($event);
+          $::log->sqlIncident($chan, $idx) if $idx;
+      }
   }
   if (defined($::sn{$nick}) && defined($::sn{$nick}->{mship})) {
     my @mship = @{$::sn{$nick}->{mship}};
@@ -673,8 +679,11 @@ sub on_mode
           my @affected = whoGotHit($chan, $ex[1]);
           if ( defined($::db) && (@affected) && (scalar @affected <= 4) ) {
             foreach my $victim (@affected) {
-              my $idx = $::db->actionlog($event, 'ban', $victim);
-              $::log->sqlIncident( $chan, $idx ) if $idx;
+              # Ignore channels that are +s and not monitored
+              if ( not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no")) ) {
+                my $idx = $::db->actionlog($event, 'ban', $victim);
+                $::log->sqlIncident( $chan, $idx ) if $idx;
+              }
             }
           }
           if ($ex[1] =~ /^\*\!\*\@(.*)$/) {
@@ -697,8 +706,11 @@ sub on_mode
           my @affected = whoGotHit($chan, $ex[1]);
           if ( defined($::db) && (@affected) && (scalar @affected <= 4) ) {
             foreach my $victim (@affected) {
-              my $idx = $::db->actionlog($event, 'quiet', $victim);
-              $::log->sqlIncident( $chan, $idx ) if $idx;
+              # Ignore channels that are +s and not monitored
+              if ( not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no")) ) {
+                my $idx = $::db->actionlog($event, 'quiet', $victim);
+                $::log->sqlIncident( $chan, $idx ) if $idx;
+              }
             }
           }
           if ($ex[1] =~ /^\*\!\*\@(.*)$/) {

commit c9f53dedf55b0f8bb6e83525dd0c25cdb7881133

Fix memory leaks and state corruption introduced by ed53ab7178841862af88f05fafe56a7b8504d527

lib/ASM/Event.pm
@@ -291,7 +291,7 @@ sub on_part
   $::log->logg( $event );
   $::db->logg( $event ) if defined $::db;
   # Ignore channels that are +s and not monitored
-  if (defined $::db and $event->{args}->[0] =~ /^requested by/ and (not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no"))) ) {
+  if (defined $::db and $event->{args}->[0] =~ /^requested by/ and (not ((grep { /^s$/ } @{$::sc{$chan}{modes}} ) && ($::channels->{channel}->{$chan}->{monitor} eq "no"))) ) {
     my $idx = $::db->actionlog( $event);
     $::log->sqlIncident($chan, $idx) if $idx;
   }
@@ -680,7 +680,7 @@ sub on_mode
           if ( defined($::db) && (@affected) && (scalar @affected <= 4) ) {
             foreach my $victim (@affected) {
               # Ignore channels that are +s and not monitored
-              if ( not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no")) ) {
+              if ( not ((grep { /^s$/ } @{$::sc{$chan}{modes}}) && ($::channels->{channel}->{$chan}->{monitor} eq "no")) ) {
                 my $idx = $::db->actionlog($event, 'ban', $victim);
                 $::log->sqlIncident( $chan, $idx ) if $idx;
               }
@@ -707,7 +707,7 @@ sub on_mode
           if ( defined($::db) && (@affected) && (scalar @affected <= 4) ) {
             foreach my $victim (@affected) {
               # Ignore channels that are +s and not monitored
-              if ( not ((grep { /^s$/ } @{$::sc{$_}{modes}}) && ($::channels->{channel}->{$_}->{monitor} eq "no")) ) {
+              if ( not ((grep { /^s$/ } @{$::sc{$chan}{modes}}) && ($::channels->{channel}->{$chan}->{monitor} eq "no")) ) {
                 my $idx = $::db->actionlog($event, 'quiet', $victim);
                 $::log->sqlIncident( $chan, $idx ) if $idx;
               }